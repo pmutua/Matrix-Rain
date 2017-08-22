8f611a987a82cb66e2c6d652c47b3073bbc3c553
pmutua <pmutua@live.com>
2017-08-22 21:56:33 +0300

    updated function to reset symbol at every nth frame and switch interval

--- js/main.js
+++ js/main.js
@@ -26,9 +26,14 @@ function Symbol(x, y, speed) {
     this.y = y;
     this.value;
     this.speed = speed;
+    this.switchInterval = round(random(2, 20));
 
     this.setToRandomSymbol = function() {
-        this.value = String.fromCharCode(0x30A0 + round(random(0, 96)));
+        if (frameCount % this.switchInterval == 0) {
+            this.value = String.fromCharCode(0x30A0 + round(random(0, 96)));
+
+        }
+
     }
 
     this.render = function() {
