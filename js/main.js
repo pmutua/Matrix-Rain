b7b3dbc1bc7348c8896be45b3f69734bea89b696
pmutua <pmutua@live.com>
2017-08-22 22:41:42 +0300

    modified main.js create multiple streams

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var symbolSize = 60;
-var stream;
+var symbolSize = 26;
+var streams = [];
 
 function setup() {
     createCanvas(
@@ -7,14 +7,26 @@ function setup() {
         window.innerHeight
     );
     background(0);
-    stream = new Stream();
-    stream.generateSymbols();
+    var x = 0;
+    var y = 0;
+    for (var i = 0; i <= width / symbolSize; i++) {
+        var stream = new Stream();
+        stream.generateSymbols(x, y);
+        streams.push(stream);
+        x += symbolSize
+    }
+
+
     textSize(symbolSize);
 }
 
 function draw() {
     background(0);
-    stream.render();
+    // render multiple streams
+    streams.forEach(function(stream) {
+        stream.render();
+    });
+
 
 }
 
@@ -66,9 +78,10 @@ function Stream() {
     this.speed = random(5, 20);
 
 
-    this.generateSymbols = function() {
-        var y = 0;
-        var x = width / 2;
+    this.generateSymbols = function(x, y) {
+        // Ive got rid of this hard coded code that i have commented 
+        // var y = 0;
+        // var x = width / 2;
 
         for (var i = 0; i <= this.totalSymbols; i++) {
             symbol = new Symbol(x, y, this.speed);
