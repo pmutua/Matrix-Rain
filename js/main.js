d46ec129991835dbfed4afbb7ad48d2ea41f86ce
pmutua <pmutua@live.com>
2017-08-22 21:02:34 +0300

    update to main.js

--- js/main.js
+++ js/main.js
@@ -6,21 +6,26 @@ function setup() {
         window.innerHeight
     );
     background(0);
+    symbol = new Symbol(
+        width / 2,
+        height / 2
+    );
+    symbol.setToRandomSymbol();
 
 }
 
 function draw() {
+    symbol.render();
 
 }
 
 function Symbol(x, y) {
     this.x = x;
     this.y = y;
-    this.value = value;
+    this.value;
 
     this.setToRandomSymbol = function() {
-        this.value = String.fromCharCode(0x30A0 + round(0, 96))
-
+        this.value = String.fromCharCode(0x30A0 + round(random(0, 96)));
     }
     this.render = function() {
         fill(0, 255, 70);
