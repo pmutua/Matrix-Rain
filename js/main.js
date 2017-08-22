9d27f9b2c7a141aba6af5275ed17b02236e4e364
pmutua <pmutua@live.com>
2017-08-22 21:35:11 +0300

    update to main.js rain function

--- js/main.js
+++ js/main.js
@@ -9,30 +9,50 @@ function setup() {
     background(0);
     symbol = new Symbol(
         width / 2,
-        height / 2
+        height / 2,
+        random(5, 10)
     );
     symbol.setToRandomSymbol();
     textSize(symbolSize);
 }
 
 function draw() {
+    background(0);
     symbol.render();
 
 }
 
-function Symbol(x, y) {
+function Symbol(x, y, speed) {
     this.x = x;
     this.y = y;
     this.value;
+    this.speed = speed;
 
     this.setToRandomSymbol = function() {
         this.value = String.fromCharCode(0x30A0 + round(random(0, 96)));
     }
+
     this.render = function() {
         fill(0, 255, 70);
         text(this.value, this.x, this.y);
+        this.rain();
     }
+
+    this.rain = function() {
+        this.y += this.speed;
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
 
 function Stream() {
 
